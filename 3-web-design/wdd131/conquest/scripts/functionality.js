44e2a910221f327c8e83cb3a1b4fe7915a67bfb0
Srose25 <ros20019@byui.edu>
2025-12-09 22:52:32 -0700

    first functionality working - info blurb

---
+++ 3-web-design/wdd131/conquest/scripts/functionality.js
@@ -0,0 +1,15 @@
+const infoToggle = document.getElementById('infoToggle');
+const infoPanel = document.getElementById('infoPanel');
+
+infoToggle.addEventListener('click', () => {
+    const panelStyle = window.getComputedStyle(infoPanel);
+    const isHidden = panelStyle.display === 'none';
+
+    if (isHidden) {
+        infoPanel.style.display = 'block';
+        infoToggle.setAttribute("aria-expanded", "true");
+    } else {
+        infoPanel.style.display = 'none';
+        infoToggle.setAttribute("aria-expanded", "false");
+    }
+});
